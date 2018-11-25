b232859568fff615b3220fd05214e8db01071e99
lawsondavid <lawsondavid@gmail.com>
2018-11-25 23:04:25 +0000

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://dave:mern123@ds115854.mlab.com:15854/daveconnector'
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+//connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch((e) => console.log(`Error connecting to mongo ${e}`));
+
+app.get('/', (req, res) => res.send('Hello'));
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+app.listen(port, () => console.log(`Server running on ${port}`));
