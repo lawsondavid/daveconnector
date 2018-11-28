e4a4becabddce2dff70cd4e3fc28672de61e12ed
lawsondavid <lawsondavid@gmail.com>
2018-11-28 17:48:09 +0000

    provide example keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
-// 'mongodb://dave:mern@ds115854.mlab.com:15854/daveconnector'
 module.exports = {
-    mongoURI: 'mongodb://dave:mern123@ds115854.mlab.com:15854/daveconnector',
-    secretOrKey: 'secret',
+    mongoURI: 'mongodb://<dbuser>:<dbpassword>@<mongohost>:<mongoport>/daveconnector',
+    secretOrKey: '<secret>',
 };
