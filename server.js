82845cb856e0d132094512378c16b5b4f020603d
lawsondavid <lawsondavid@gmail.com>
2018-11-28 13:44:32 +0000

    point to build folder in production

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,5 +31,13 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 app.listen(port, () => console.log(`Server running on ${port}`));
