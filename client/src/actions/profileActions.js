f0b7a2b4000f8ca04b99bd34a907768ac334e85d
lawsondavid <lawsondavid@gmail.com>
2018-11-28 10:12:55 +0000

    add delete profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {CLEAR_CURRENT_PROFILE, GET_PROFILE, PROFILE_LOADING} from "./types";
+import {CLEAR_CURRENT_PROFILE, GET_ERRORS, GET_PROFILE, PROFILE_LOADING, SET_CURRENT_USER} from "./types";
 
 export const getCurrentProfile = () => dispatch => {
     dispatch(setProfileLoading());
@@ -18,6 +18,18 @@ export const getCurrentProfile = () => dispatch => {
         );
 };
 
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            });
+        })
+};
+
 export const setProfileLoading = () => {
     return {
         type: PROFILE_LOADING
@@ -27,5 +39,21 @@ export const setProfileLoading = () => {
 export const clearCurrentProfile = () => {
     return {
         type: CLEAR_CURRENT_PROFILE
+    }
+};
+
+//deletes both profile and user
+export const deleteAccount = () => dispatch => {
+    if (window.confirm('Are you sure?')) {
+        axios.delete('/api/profile')
+            .then(res => dispatch({
+                type: SET_CURRENT_USER,
+                payload: {}
+            }))
+            .catch(err =>
+                dispatch({
+                    type: GET_ERRORS, payload: err.response.data
+                })
+            )
     }
 };
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {getCurrentProfile} from "../../actions/profileActions";
+import {getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from '../common/Spinner'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import ProfileActions from './ProfileActions'
 
 class Dashboard extends Component {
     componentDidMount() {
         this.props.getCurrentProfile();
+    }
+
+    onDeleteClick(e){
+        this.props.deleteAccount();
     }
 
     render() {
@@ -19,7 +24,16 @@ class Dashboard extends Component {
             dashboardContent = <Spinner/>
         } else {
             if (Object.keys(profile).length > 0) {
-                dashboardContent = <h1>TODO DISPLAY PROFILE</h1>
+                dashboardContent = (
+                    <div>
+                        <p className="lead text muted">
+                            Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+                        </p>
+                        <ProfileActions/>
+                        <div style={{ marginBottom: '60px'}}/>
+                        <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>
+                    </div>
+                )
             } else {
                 //logged in but has not created a profile
                 dashboardContent = (
@@ -51,6 +65,7 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -60,4 +75,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(Dashboard);
