198a78f5f021d17d734b8925e7e9dbf21e5dc3b4
lawsondavid <lawsondavid@gmail.com>
2018-11-28 11:56:09 +0000

    add experience and education

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import {clearCurrentProfile} from "./actions/profileActions";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 if (localStorage.jwtToken) {
     setAuthToken(localStorage.jwtToken);
@@ -52,6 +54,12 @@ class App extends Component {
                             </Switch>
                             <Switch>
                                 <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path="/add-education" component={AddEducation}/>
                             </Switch>
                         </div>
                         <Footer/>
